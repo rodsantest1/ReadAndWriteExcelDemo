f2884a27b7e1226292105d55c8de1e3b8e593d4c
Rodney Santiago <GD000007711@ups.com>
2021-09-29 19:05:53 -0400

    Add project files.

---
+++ ReadAndWriteExcelApp/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ReadAndWriteExcelApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.txtWrite = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRead = new System.Windows.Forms.TextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(164, 230);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 0;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // txtWrite
+            // 
+            this.txtWrite.Location = new System.Drawing.Point(153, 188);
+            this.txtWrite.Name = "txtWrite";
+            this.txtWrite.Size = new System.Drawing.Size(100, 20);
+            this.txtWrite.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(298, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 55);
+            this.label1.TabIndex = 2;
+            this.label1.Text = ">> >> >>";
+            // 
+            // txtRead
+            // 
+            this.txtRead.Location = new System.Drawing.Point(560, 188);
+            this.txtRead.Name = "txtRead";
+            this.txtRead.Size = new System.Drawing.Size(100, 20);
+            this.txtRead.TabIndex = 3;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(571, 230);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 4;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.txtRead);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWrite);
+            this.Controls.Add(this.btnWrite);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.TextBox txtWrite;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRead;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
+
---
+++ ReadAndWriteExcelApp/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ReadAndWriteExcelApp
+{
+    public partial class Form1 : Form
+    {
+        private string FileName = @"C:\Users\SBJ9DCH\source\repos\ReadAndWriteExcelSol\ReadAndWriteExcelApp\bin\Debug\data.xlsx";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            // Set cursor as hourglass
+            Cursor.Current = Cursors.WaitCursor;
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(FileName);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            xlWorksheet.Cells[1, 1] = txtWrite.Text;
+            xlApp.Visible = false;
+            xlApp.UserControl = false;
+            xlWorkbook.Save();
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+            // Set cursor as default arrow
+            Cursor.Current = Cursors.Default;
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            // Set cursor as hourglass
+            Cursor.Current = Cursors.WaitCursor;
+
+            Excel.Application xlApp = new Excel.Application();
+            //xlApp.Visible = true;
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(FileName);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            if (xlRange.Cells[1, 2] != null && xlRange.Cells[1, 2].Value2 != null)
+            {
+                txtRead.Text = xlRange.Cells[1, 2].Value2.ToString();
+            }
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+            // Set cursor as default arrow
+            Cursor.Current = Cursors.Default;
+        }
+    }
+}
